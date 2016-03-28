b334265dd0838409fa86d354f10d01169dffe2d6
Justin Nguyen <jnguyen3950@gmail.com>
2016-03-28 13:49:39 -0700

    User can now view video that is linked to the page. #2

---
+++ client.js
@@ -0,0 +1,22 @@
+var player = document.getElementById('player');
+var search = document.getElementById('search');
+
+search.addEventListener('click', function(event) {
+  event.preventDefault();
+  var searchBox = document.getElementById('searchBox');
+  var data = {key: searchBox.value};
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/search');
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(JSON.stringify(data));
+
+  xhr.addEventListener('load', function() {
+    var response = JSON.parse(xhr.responseText);
+    // console.log(response[0]);
+    // console.log(response[0].link);
+    var url = _.propertyOf(response[0])('link');
+    url = url.replace("watch?v=", "v/");
+    player.setAttribute('src', url);
+  })
+})
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var jsonParser = require('body-parser').json();
+var search = require('youtube-search');
+
+app.post('/search', jsonParser, function(req, res) {
+  console.log(req.body.key);
+
+  var opts = {
+    maxResults: 1,
+    key: 'AIzaSyDZ9sbX9zra9vN5WUjxMAQCf_5j01pHqVM'
+  };
+
+  search(req.body.key, opts, function(err, results) {
+    if(err) return console.log(err);
+    console.dir(results);
+    console.dir(results.id);
+    res.send(results);
+  });
+})
+
+app.use(express.static('./'));
+
+app.listen(8080);
