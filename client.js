066ea73ecc4013e4326b8e59f5695ab651e0040b
Justin Nguyen <jnguyen3950@gmail.com>
2016-03-29 13:10:48 -0700

    #3 Page will now generate media block according to the number of the video comments.

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 var player = document.getElementById('player');
+var comment = document.getElementById('comment');
 var search = document.getElementById('search');
 var searchBox = document.getElementById('searchBox');
 var searchResult = document.getElementById('searchResult');
@@ -12,29 +13,12 @@ searchBox.addEventListener('keypress', function(event) {
     searchData();
     event.preventDefault();
   }
-  // viewVideo();
 })
-
-function viewVideo() {
-  var nodeList = document.getElementsByClassName('videoBlock');
-  console.log(nodeList);
-  console.log(nodeList.length);
-  for (var i = 0; i < nodeList.length; i++) {
-    nodeList[i].addEventListener('click', function(event) {
-      if (event.target.classList.contains('img')) {
-        var link = event.target.getAttribute('data-link');
-        link = link.replace("watch?v=", "v/");
-        player.setAttribute('src', link);
-      }
-      console.log(event.target);
-    })
-  }
-}
 
 function searchData() {
   clearResult(searchResult);
 
-  var data = {key: searchBox.value};
+  var data = {term: searchBox.value};
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/search');
   xhr.setRequestHeader('Content-type', 'application/json');
@@ -42,13 +26,12 @@ function searchData() {
 
   xhr.addEventListener('load', function() {
     var response = JSON.parse(xhr.responseText);
+    console.log(response[0]);
     for (var i = 0; i < response.length; i++) {
       addThumbnail(response[i]);
-      // console.log(response[i]);
-      // console.log(response[i].thumbnails.high.url);
     }
+    attachThumbnailListener();
   })
-  viewVideo();
 }
 
 function addThumbnail(object) {
@@ -77,7 +60,73 @@ function addThumbnail(object) {
   caption.setAttribute('class', 'caption');
   addPlaylist.setAttribute('class', 'btn btn-default');
   addPlaylist.setAttribute('role', 'button');
+
   searchResult.appendChild(videoBlock);
+}
+
+function attachThumbnailListener() {
+  var nodeList = document.getElementsByClassName('videoBlock');
+  for (var i = 0; i < nodeList.length; i++) {
+    nodeList[i].addEventListener('click', function(event) {
+      var link = event.target.getAttribute('data-link');
+      link = link.replace("watch?v=", "v/");
+      player.setAttribute('src', link);
+    })
+  }
+}
+
+var commentButton = document.getElementById('commentButton');
+commentButton.addEventListener('click', function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/view');
+  xhr.send();
+
+  xhr.addEventListener('load', function() {
+    var response = JSON.parse(xhr.response);
+    console.log(response.comments);
+
+    var commentArray = response.comments;
+    for (var i = 0; i < commentArray.length; i++) {
+      addMedia(commentArray);
+    }
+
+  })
+})
+
+// <div class="media">
+//   <div class="media-left">
+//     <a href="#"><img class="media-object" style="width: 64px; height: 64px;"></a>
+//   </div>
+//   <div class="media-body">
+//     <h4 class="media-heading">Another media heading</h4>
+//     Say more things.
+//   </div>
+// </div>
+
+function addMedia(array) {
+  var mediaBlock = document.createElement('div');
+  var mediaLeft = document.createElement('div');
+  var image = document.createElement('img');
+  var mediaBody = document.createElement('div');
+  var userName = document.createElement('h3');
+  var userNameText = document.createTextNode('');
+  var commentText = document.createTextNode('');
+
+  mediaBlock.appendChild(mediaLeft);
+  mediaLeft.appendChild(image);
+  mediaBlock.appendChild(mediaBody);
+  userName.appendChild(userNameText);
+  mediaBody.appendChild(commentText);
+
+  mediaBlock.setAttribute('class', 'media');
+  mediaLeft.setAttribute('class', 'media-left');
+  image.setAttribute('class', 'media-object');
+  image.setAttribute('src', '');
+  image.setAttribute('style', 'width: 64px; height: 64px;');
+  mediaBody.setAttribute('class', 'media-body');
+  userName.setAttribute('class', 'media-heading');
+
+  comment.appendChild(mediaBlock);
 }
 
 function clearResult(result) {
--- server.js
+++ server.js
@@ -2,22 +2,40 @@ var express = require('express');
 var app = express();
 var jsonParser = require('body-parser').json();
 var search = require('youtube-search');
+var fetchCommentPage = require('youtube-comment-api')();
 
 app.post('/search', jsonParser, function(req, res) {
-  console.log(req.body.key);
+  console.log(req.body.term);
 
   var opts = {
     maxResults: 12,
-    key: 'AIzaSyDZ9sbX9zra9vN5WUjxMAQCf_5j01pHqVM'
+    key: 'AIzaSyDZ9sbX9zra9vN5WUjxMAQCf_5j01pHqVM',
+    order: 'viewCount',
+    type: 'video',
+    videoType: 'movie',
   };
 
-  search(req.body.key, opts, function(err, results) {
+  search(req.body.term, opts, function(err, results) {
     if(err) return console.log(err);
-    console.dir(results);
-    console.dir(results.id);
+    // console.dir(results);
+    // console.dir(results.id);
     res.send(results);
   });
-})
+
+});
+
+app.get('/view', function(req, res) {
+  fetchCommentPage('KuvWdZ3SJuQ').then(function (commentPage) {
+    console.log(commentPage);
+    res.send(commentPage);
+    // return commentPage.nextPageToken;
+  // }).then(function (pageToken) {
+  //   // request next page
+  //   return fetchCommentPage('KuvWdZ3SJuQ', pageToken)
+  // }).then(function (commentPage) {
+  //   console.log(commentPage);
+  });
+});
 
 app.use(express.static('./'));
 
